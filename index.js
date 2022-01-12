2aa3021f8efb1f20ce8ce612cc5d199b9a16f40c
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-11 18:20:59 -0600

    commit tree ae513d4a862369692b11fde89321153ae1db173f

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-// Code your solution in this file!
+// // Code your solution in this file!
 
-function returnFirstTwoDrivers(drivers) {
-    return drivers.slice(0,2);
+// function returnFirstTwoDrivers(drivers) {
+//     return drivers.slice(0,2);
+// }
+
+// function returnLastTwoDrivers(drivers) {
+//     return drivers.slice(-2)
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// function createFareMultiplier(multiplier) {
+//     return function(number) {return multiplier * number};
+// }
+
+// const fareDoubler = createFareMultiplier(2);
+
+// const fareTripler = createFareMultiplier(3);
+
+
+
+// function selectDifferentDrivers(arrayOfDrivers, action) {
+//     return action(arrayOfDrivers);
+// }
+
+
+
+
+
+const returnFirstTwoDrivers = function(array) {
+    const drivers = array.slice(0,2)
+    return drivers
 }
 
-function returnLastTwoDrivers(drivers) {
-    return drivers.slice(-2)
+const returnLastTwoDrivers = function(array) {
+    const drivers = array.slice(-2)
+    return drivers
 }
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-
-function createFareMultiplier(multiplier) {
-    return function(number) {return multiplier * number};
-}
-
-const fareDoubler = createFareMultiplier(2);
-
-const fareTripler = createFareMultiplier(3);
-
-
-
-function selectDifferentDrivers(arrayOfDrivers, action) {
-    return action(arrayOfDrivers);
-}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
